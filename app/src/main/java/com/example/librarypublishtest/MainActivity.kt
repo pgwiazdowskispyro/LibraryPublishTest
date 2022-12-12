92098a6cd3b2dd3a8a6237caf2b8b17f00b9c042
Patryk Gwiazdowski <pgw@spyro-soft.com>
2022-12-12 15:50:07 +0100

    initial commit

---
+++ app/src/main/java/com/example/librarypublishtest/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.librarypublishtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val text = findViewById<TextView>(R.id.text)
+        text.setText(com.gwiazdowski.strings.R.string.text)
+    }
+}
